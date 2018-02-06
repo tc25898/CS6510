d74d81723141f0a5fdffa9ceb5423d8d4feffb7c
tc25898 <740419913@qq.com>
2018-02-06 13:50:33 -0800

    add test

---
+++ tests/com/example/servlets/ServletTest.java
@@ -0,0 +1,21 @@
+package com.example.servlets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by c on 2/6/18.
+ */
+public class ServletTest {
+    @Test
+    public void doPost() throws Exception {
+        assertEquals(0,0);
+    }
+
+    @Test
+    public void doGet() throws Exception {
+        assertEquals(0,0);
+    }
+
+}
